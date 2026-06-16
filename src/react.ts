d2a3bf4ec0dd7b6f4f230168bcf507935e09c2d5
Juan Miguel Ruiz Pazos <juanmiguelruizpazos@gmail.com>
2026-06-16 17:14:33 +0200

    refactor: streamline ESLint configuration by consolidating plugin settings into an array

--- src/react.ts
+++ src/react.ts
@@ -5,14 +5,15 @@ import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 import baseConfig from './index';
 import { reactRules } from './rules/react';
 
+const pluginConfigs = [
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  reactHooksPlugin.configs.flat.recommended,
+].filter((config) => config != null);
+
 export default tseslint.config(
   ...baseConfig,
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  reactPlugin.configs.flat.recommended!,
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  reactPlugin.configs.flat['jsx-runtime']!,
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  reactHooksPlugin.configs.flat.recommended!,
+  ...pluginConfigs,
   jsxA11yPlugin.flatConfigs.recommended,
   {
     settings: { react: { version: 'detect' } },
